0b254bec7770c4cc73412636edd370026d78bf68
Minjae Shin <alswoalswo3570@gmail.com>
2026-04-23 14:12:59 +0900

    revert: back to @libsql/client

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
